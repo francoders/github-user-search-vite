310123830f15f7bf50229b3f3c072993e9d83d36
Francisco Corvalan <francisco22corvalan@gmail.com>
2022-07-08 23:53:17 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Container } from "@mui/system";
+import React from "react";
+import { Search } from "./components";
+
+export default function App() {
+	return (
+		<Container>
+			<h1>Github User Search</h1>
+		</Container>
+	);
+}
